3012e92c22405e0a60e86f841c96874f7eb7f818
Kylie Verzosa <verzosakylie1112@gmail.com>
2026-05-12 19:42:43 +0800

    Fix country flags to use flagcdn images for windows compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from app.config import Config
 from app.extensions import db, migrate, csrf, login
 from app.forms import LogoutForm
 from app.routes import main_bp
-from app.routes.main_routes import country_to_flag
 
 
 def create_app() -> Flask:
@@ -20,7 +19,6 @@ def create_app() -> Flask:
     from app import models
 
     app.register_blueprint(main_bp)
-    app.jinja_env.filters['flag'] = country_to_flag
 
     @app.context_processor
     def inject_logout_form():
