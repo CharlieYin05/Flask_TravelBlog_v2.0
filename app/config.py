d53b32c03c836a4b70290f28cd21f7914122bf96
Charlie Yin <yinchangcan026@outlook.com>
2026-07-15 09:52:23 +0800

    support DATABASE_URL configuration. Append PyMySQL in requirements.txt

--- app/config.py
+++ app/config.py
@@ -6,7 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'app.db'}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{BASE_DIR / 'app.db'}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY") # "your-google-maps-api-key"
     
